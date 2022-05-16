a352be4fe69e4eeac610c7669ac0be61beda1052
Vinicius Gomes <98960590+viniciusg34@users.noreply.github.com>
2022-05-16 18:06:06 -0300

    50% do login já foi feito

--- src/Pages/login/login.tsx
+++ src/Pages/login/login.tsx
@@ -1,4 +1,5 @@
 import '../../Assets/CSS/login.css'
+import logo from '../../Assets/img/logo.png';
 
 export const Login = ():JSX.Element =>{
 
@@ -6,7 +7,22 @@ export const Login = ():JSX.Element =>{
         <>
             <div className='centralizarLogin'>
                 <div className='login'>
-                    
+                    <div className='centralizarLogo'>
+                      <img src={logo} alt="logo"/>
+                    </div>
+                    <section className='loginCadastrar'>
+                      <div className='cardLogin'>
+                        <h2 className='tituloLogin'>Login</h2>
+                        <p className='textoLogin'>Este portal oferece uma experiência única, segura e oferta um serviço de qualidade!</p>
+
+                      </div>
+                      <div className='cardCadastrar'>
+                        <h2 className='tituloCadastrar'>Cadastrar</h2>
+                        <input placeholder='Nome Completo' className='inputCadastrar' type="text"/>
+                        <input placeholder='Telefone' className='inputCadastrar' type="text"/>
+                        <input placeholder='E-mail' className='inputCadastrar' type="text"/>
+                      </div>
+                    </section>
                 </div>
             </div>
         </>
