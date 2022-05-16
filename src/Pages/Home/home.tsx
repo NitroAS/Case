c46dfdc12f57ef0b5b72a9a93d53ef58b139b16d
josebrokenh01 <99042143+josebrokenh01@users.noreply.github.com>
2022-05-16 18:47:04 -0300

    jose aqui

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,15 +1,9 @@
-import { Link } from 'react-router-dom';
 import '../../Assets/CSS/home.css'
-import { Header } from '../../Components/Header/header'
 
-export const Home = ():JSX.Element =>{
+export const Home = (): JSX.Element =>{
 
     return(
         <>
-          <Header />
-
-            <Link className="button1" to='/'>Home</Link>
-            <Link className="button2" to='/perfil'>Perfil</Link>
 
         </>
     );
