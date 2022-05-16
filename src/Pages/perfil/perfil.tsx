6a233e3868cfba99392d34718ee4d72b1a62eaff
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-16 17:14:23 -0300

    Header- fazendo

--- src/Pages/perfil/perfil.tsx
+++ src/Pages/perfil/perfil.tsx
@@ -1,9 +1,11 @@
 import '../../Assets/CSS/perfil.css'
+import { Header } from '../../Components/Header/header'
 
 export const Perfil = ():JSX.Element =>{
 
     return(
         <>
+            <Header />
           
         </>
     );
