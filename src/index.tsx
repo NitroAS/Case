2f6afb0e605966f7c518b24115b552c8149a7fd8
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-16 16:13:45 -0300

    Pasts

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom';
+import './Assets/CSS/global.css'
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+
+const Rotas = () =>{
+
+  return(
+    <BrowserRouter>
+          <Routes>
+            
+             
+          </Routes>
+      </BrowserRouter>
+  );
+  
+}
+
+ReactDOM.render(<Rotas/> , document.getElementById('root'));
+
+
+
