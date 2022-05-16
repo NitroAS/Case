307dc25b5ffb0182ecdf2fd61fd8dba9de3b15ac
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-16 17:15:31 -0300

    Jose criado branch

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,10 +1,12 @@
 import '../../Assets/CSS/home.css'
+import { Header } from '../../Components/Header/header'
 
 export const Home = ():JSX.Element =>{
 
     return(
         <>
-          
+          <Header />
+
         </>
     );
 }
