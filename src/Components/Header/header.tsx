85139a314868920d6d040a8c5e450b42e7b57c6e
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-16 17:27:42 -0300

    Header arrumando

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,12 +1,13 @@
 import '../../Assets/CSS/header.css'
 import { Link } from "react-router-dom";
-
+import HeaderFoto from ''
 export const Header = (): JSX.Element => {
 
     return (
         <>
-            <Link className="button1" to='/'>Home</Link>
-            <Link className="button2" to='/Pag2'>Usuários</Link>
+            <div className="LogoHeader">
+                <img src={HeaderFoto} alt="Logo do header" />
+            </div>
         </>
     );
 }
