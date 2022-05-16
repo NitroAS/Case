f319ef0b3d907f4c42b7fa5f6cd44375744b1db0
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-16 17:11:43 -0300

    Fazendo Header

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,12 +1,18 @@
 import '../../Assets/CSS/header.css'
 import { Link } from "react-router-dom";
-
+import HeaderFoto from "../../Assets/img/Group_1.png"
 export const Header = (): JSX.Element => {
 
     return (
         <>
-            <Link className="button1" to='/'>Home</Link>
-            <Link className="button2" to='/Pag2'>Usuários</Link>
+
+            <div className="LogoHeader">
+                <img src={HeaderFoto} alt="Logo do header" />
+            </div>
+
+
+            {/* <Link className="button1" to='/'>Home</Link>
+            <Link className="button2" to='/Pag2'>Usuários</Link> */}
         </>
     );
 }
