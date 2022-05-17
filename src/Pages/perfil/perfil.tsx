4f7f4fba0c169e6a016b1feee3c638284ea2e0e2
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-17 14:21:00 -0300

    Perfil pag fazendo

--- src/Pages/perfil/perfil.tsx
+++ src/Pages/perfil/perfil.tsx
@@ -1,16 +1,49 @@
 import '../../Assets/CSS/perfil.css'
 import { Header } from '../../Components/Header/header'
 
-export const Perfil = ():JSX.Element =>{
+export const Perfil = (): JSX.Element => {
 
-    return(
+    return (
         <>
             <Header />
-            
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo vel commodi nihil dolorem veritatis est, similique voluptates dignissimos architecto nemo. Impedit incidunt velit nulla perspiciatis quidem dicta laboriosam eveniet. Modi.
-            </p>
+
+
+            <section className='AlinhamentoTotal'>
+                <div className='BoxPerfil'>
+
+                    <h1>Perrfil</h1>
+
+                    <div className="InputPerfil">
+
+                        <input className="inputsPerfil"
+                            type="text" name="InputPerfil"
+                            placeholder="Maria de Fátma Muniz"
+                            maxLength={40}></input>
+
+                        <input className="inputsPerfil"
+                            type="text" name="InputPerfil"
+                            placeholder="(11) 9999-9090"
+                            maxLength={40}></input>
+
+                        <input className="inputsPerfil"
+                            type="text" name="InputPerfil"
+                            placeholder="maria@gmail.com"
+                            maxLength={40}></input>
+
+
+                        <div className="btnPerfilAlinhamento">
+                            <button className='btnPerfil'>Atualizar</button>
+                            <button className='btnPerfil'>Excluir Cadastro</button>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </section>
+
         </>
     );
 }
- 
+
 export default Perfil 
