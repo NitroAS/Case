57be92c44c5f1209683ce11bc0abadbdf03dd473
ArloSindorff <98981849+ArloSindorff@users.noreply.github.com>
2022-05-17 11:53:52 -0300

    Colocanco header e footer na home para uma melhor vizualização

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,12 +1,16 @@
 import '../../Assets/CSS/home.css'
 
+// Header e Footer
+import Header from '../../Components/Header/header';
+import Footer from '../../Components/Footer/footer';
+
 export const Home = ():JSX.Element =>{
 
     return(
         <>
-          
+          <Header></Header>
            
-
+          <Footer></Footer>  
         </>
     );
 }
