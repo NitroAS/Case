bda668190db7c333d4a4a1ae2feb27d1ed5f0f64
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-17 14:04:15 -0300

    TSX do Header jogando para branch alex

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,15 +1,30 @@
 import '../../Assets/CSS/header.css'
 import { Link } from "react-router-dom";
-import LogoHeader from '../../Assets/img/logo.png'
+import LogoHeader from "../../Assets/img/logo.png"
 export const Header = (): JSX.Element => {
 
     return (
         <>
-            <div className="LogoHeader">
-                <img src={LogoHeader} alt="Logo do header" />
+
+            <div className="AlinhamentoHeader">
+               
+                    <Link className="buttonHeader" to='/'>Home</Link>
+                    <Link className="buttonHeader" to='/carros'>Carros</Link>
+                    <Link className="buttonHeader" to='/reservas'>Reservas</Link>
+               
+
+                <div className="LogoHeader">
+                    <img src={LogoHeader} alt="Logo do header" />
+                </div>
+
+                    <Link className="buttonHeader" to='/locadora'>Locadora</Link>
+                    <Link className="buttonHeader" to='/perfil'>Perfil</Link>
+                    <Link className="buttonHeader" to='/login'>Sair</Link>
+                
             </div>
 
-            <Link className="button1" to='/'>Home</Link>
+
+
         </>
     );
 }
