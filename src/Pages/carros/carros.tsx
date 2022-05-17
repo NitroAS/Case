c831bb800a922b053ff660b10bd4154f943cb522
ArloSindorff <98981849+ArloSindorff@users.noreply.github.com>
2022-05-17 15:21:47 -0300

    Apenas mudanças básicas

--- src/Pages/carros/carros.tsx
+++ src/Pages/carros/carros.tsx
@@ -1,7 +1,27 @@
 export const Carros = ():JSX.Element => {
     return(
         <div>
-            
+            <section>
+                <h1>Carros</h1>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <button>Cadastrar</button>
+            </section>
+            <section>
+                <h2>Econômico</h2>
+                <div className="barraDeSeparacaoCarros"></div>
+            </section>
+            <section>
+                <h2>Especial</h2>
+                <div className="barraDeSeparacaoCarros"></div>
+            </section>
+            <section>
+                <h2>Luxo</h2>
+                <div className="barraDeSeparacaoCarros"></div>
+            </section>
         </div>
     )
 }
