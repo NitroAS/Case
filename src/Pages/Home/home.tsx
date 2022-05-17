48f5276820552c8bfd13a06dbfce4f1f6fe5b7f8
josebrokenh01 <99042143+josebrokenh01@users.noreply.github.com>
2022-05-17 17:34:59 -0300

    Terminei Css e Html da HOME fml

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,9 +1,101 @@
 import '../../Assets/CSS/home.css'
 
-export const Home = (): JSX.Element =>{
+import Logo from '../../Assets/img/logo.png'
+import PFoto from '../../Assets/img/banner-car 1.png'
+import SFoto from '../../Assets/img/car1 1.png'
+import TFoto from '../../Assets/img/car2 1.png'
+import QFoto from '../../Assets/img/car3 2.png'
+
+export const Home = (): JSX.Element => {
 
     return(
-        <>
+        <> 
+            <header>
+
+                <main>
+
+                    <section className='PagMain'>
+
+                        <div className='ImgPrincipal'>
+
+                            <div>
+
+                                <div className='centralizarLogo'>
+
+                                    <img className='logo' src={Logo} alt = ""/>
+
+                                </div>
+
+                                <img className='PrimeiraFoto' src={PFoto} alt = ""/>
+
+                                <div className='botoes1'>
+
+                                    <button className='LoginB'>Login</button>
+
+                                    <button className='CadastrarB'>Cadastrar</button> 
+
+
+
+                                </div>
+
+
+                            </div>
+
+
+
+                        </div>
+
+
+
+
+
+                        <div className='cards'>
+
+                            <div className='Carros'>
+
+                                <img className='1FtDeBaixo' src={SFoto} alt="" />
+
+                                <h3 className='textoH3'>Econômico</h3>
+
+                                <button className='ReservarB'>Reservar</button>
+
+                            </div>
+
+                            <div className='Carros'>
+
+                                <img src={TFoto} alt="" />
+
+                                <h3 className='textoH3'>Especial</h3>
+
+                                <button className='ReservarB'>Reservar</button>
+
+                            </div>
+
+                            <div className='Carros'>
+
+                                <img src={QFoto} alt="" />
+
+                                <h3 className='textoH3'>Luxo</h3>
+
+                                <button className='ReservarB'>Reservar</button>
+
+                            </div>
+
+                        </div>
+                        
+
+
+
+
+
+
+
+                    </section>
+
+                </main>
+
+            </header>
+
 
         </>
     );
