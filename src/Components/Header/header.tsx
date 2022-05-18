f9a6db7350899a4cf638a70cdefeb69681f22185
ArloSindorff <98981849+ArloSindorff@users.noreply.github.com>
2022-05-18 17:02:07 -0300

    Primeiro ajuste header e footer

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -5,22 +5,23 @@ export const Header = (): JSX.Element => {
 
     return (
         <>
-
-            <div className="AlinhamentoHeaderPerfil">
-               
-                    <Link className="btnHeaderPerfil" to='/'>Home</Link>
-                    <Link className="btnHeaderPerfil" to='/carros'>Carros</Link>
-                    <Link className="btnHeaderPerfil" to='/reservas'>Reservas</Link>
-               
-
-                <div className="LogoHeader">
-                    <img src={LogoHeader} alt="Logo do header" />
-                </div>
-
-                    <Link className="btnHeaderPerfil" to='/locadora'>Locadora</Link>
-                    <Link className="btnHeaderPerfil" to='/perfil'>Perfis</Link>
-                    <Link className="btnHeaderPerfil" to='/login'>Sair</Link>
+            <div className='header'>
+                <div className="AlinhamentoHeaderPerfil">
                 
+                        <Link className="btnHeaderPerfil" to='/'>Home</Link>
+                        <Link className="btnHeaderPerfil" to='/carros'>Carros</Link>
+                        <Link className="btnHeaderPerfil" to='/reservas'>Reservas</Link>
+                
+
+                    <div className="LogoHeader">
+                        <img src={LogoHeader} alt="Logo do header" />
+                    </div>
+
+                        <Link className="btnHeaderPerfil" to='/locadora'>Locadora</Link>
+                        <Link className="btnHeaderPerfil" to='/perfil'>Perfis</Link>
+                        <Link className="btnHeaderPerfil" to='/login'>Sair</Link>
+                    
+                </div>
             </div>
 
 
