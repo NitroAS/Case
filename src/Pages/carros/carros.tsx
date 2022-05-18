e701619d486dfdc9adb0e0d514dbe2c811e0de30
ArloSindorff <98981849+ArloSindorff@users.noreply.github.com>
2022-05-18 13:30:46 -0300

    Página Carros sem a API

--- src/Pages/carros/carros.tsx
+++ src/Pages/carros/carros.tsx
@@ -4,36 +4,52 @@ import '../../Assets/CSS/carros.css'
 export const Carros = ():JSX.Element => {
     return(
         <div>
-            <section>
-                <h1>Carros</h1>
-                <input type="text" placeholder='Ex.: Onix 2.0'/>
-                <select>
-                    <option selected disabled hidden>Airbag</option>
-                    <option value="false">Sem Airbag</option>
-                    <option value="true">Com Airbag</option>
-                </select>
-                <input type="text" placeholder="Portas:" />
-                <input type="text" placeholder="N° de Pessoas" />
-                <select>
-                    <option selected disabled hidden>Qual a Locadoura?</option>
-                    <option>Loucadora 1</option>
-                    <option>Loucadora 2</option>
-                    <option>Loucadora 3</option>
-                </select>
-                <button>Cadastrar</button>
-            </section>
-            <section>
-                <h2>Econômico</h2>
-                <div className="barraDeSeparacaoCarros"></div>
-            </section>
-            <section>
-                <h2>Especial</h2>
-                <div className="barraDeSeparacaoCarros"></div>
-            </section>
-            <section>
-                <h2>Luxo</h2>
-                <div className="barraDeSeparacaoCarros"></div>
-            </section>
+            <div className='mainCarros'>
+                <div className='alinhamentoMainCarros'>
+                    <section>
+                        <div className='alinhamentoSectionUmCarros'>
+                            <div className='alinhamentoCadastrarCarros'>
+                                <div className='divH1Carros'>
+                                    <h1>Carros</h1>
+                                </div>
+                                <div className='divInputMaior'>
+                                    <input type="text" placeholder='Ex.: Onix 2.0' className='escolherTiposDeCarros'/>
+                                </div>
+                                <div className='inputsMenores'>
+                                    <select className='selectBooleanoAirbag'>
+                                        <option selected disabled hidden>Airbag</option>
+                                        <option value="false">Sem Airbag</option>
+                                        <option value="true">Com Airbag</option>
+                                    </select>
+                                    <input type="text" placeholder="Portas:" className='inputsMenoresCarros' />
+                                    <input type="text" placeholder="N° de Pessoas" className='inputsMenoresCarros' />
+                                </div>
+                                <div className='divCadastrarCarros'>
+                                    <select className='selectLocadouraCarros'>
+                                        <option selected disabled hidden>Qual a Locadoura?</option>
+                                        <option>Loucadora 1</option>
+                                        <option>Loucadora 2</option>
+                                        <option>Loucadora 3</option>
+                                    </select>
+                                    <button className='buttonCadastrarCarros'>Cadastrar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className='sectionsDoscardscarros'>
+                        <h2 className='h2Carros'>Econômico</h2>
+                        <div className="barraDeSeparacaoCarros"></div>
+                    </section>
+                    <section className='sectionsDoscardscarros'>
+                        <h2 className='h2Carros'>Especial</h2>
+                        <div className="barraDeSeparacaoCarros"></div>
+                    </section>
+                    <section className='sectionsDoscardscarros'>
+                        <h2 className='h2Carros'>Luxo</h2>
+                        <div className="barraDeSeparacaoCarros"></div>
+                    </section>
+                </div>
+            </div>
         </div>
     )
 }
