06a83f4ab568c14426e5e1148111652b9363855a
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-19 19:31:52 -0300

    listar e Excluir base - FEITA OBS:DEIXEI COMENTADO PRA N TER IMPEDIMENTOS

--- src/Pages/Reserva/reserva.tsx
+++ src/Pages/Reserva/reserva.tsx
@@ -2,6 +2,8 @@ import '../../Assets/CSS/reserva.css'
 import { Header } from '../../Components/Header/header'
 import { Footer } from '../../Components/Footer/footer'
 import carroAzul from '../../Assets/img/economico.png'
+// import { apiCase } from '../../services/api'
+// import { useEffect, useState } from 'react'
 let propsReserva: any = {
     descriptionHome:  'Home',
     descriptionCarros:  'Carros',
@@ -13,13 +15,63 @@ let propsReserva: any = {
 }
 
 export const Reserva = (): JSX.Element => {
+    // const [reserva, setReserva] = useState<any[]>([]);
+    // const [usuario, setUsuario] = useState<any[]>([]);
 
+    // const PegandoReservas = () => {
+
+    //     apiCase.get('reservas')
+    //         .then(resultado => {
+
+    //             setReserva(resultado.data)
+
+    //         })
+
+           
+
+    // }
+
+    // useEffect(() => {
+
+    //     PegandoReservas()
+    // }, [])
+
+    // useEffect(() =>{
+    //     let quantidadeElementos: number = usuario.length
+
+    //     for (let index = 0; index < quantidadeElementos; index++) {
+          
+    //         apiCase.get(`Usuario/${index + 1}/ReservaS?_expand=carro`)
+    //             .then(resultadoReservas => {
+    //                 setUsuario( reserva => reserva.concat(resultadoReservas.data))
+    //             })
+            
+    //     }
+    // },    [usuario.length])
+
+    // useEffect(() => {
+    // },    [reserva.length])
+    
+    
+
+
+    // const ExcluirReservas = (id: any , usuarioId: number)  => {
+    //     if (window.confirm('Deseja realmente excluir o Perfil?')) {
+    //         apiCase.delete(`reservas/${id}/ReservaS?_expand=carro/${usuarioId}`)
+    //             .then(() => {
+    //                 window.location.reload()
+
+    //             })
+    //         }
+            
+    // }
     return (
         <>
             <Header objeto={propsReserva} />
+
+       
             <div className='alinhamentoAereoMain'>
                 <div className='mainReserva'>
-
                     <section>
 
                         <h1 className='reservaText'>Reservas</h1>
@@ -39,6 +91,7 @@ export const Reserva = (): JSX.Element => {
                                 <p className='informacoesReserva'>Data retirada: 22/05/2022</p>
                                 <p className='informacoesReserva'>Horário retirada: 16:30</p>
                                 <p className='informacoesReserva'>Data de devolução:  30/05/2022</p>
+         
 
                             </div>
 
