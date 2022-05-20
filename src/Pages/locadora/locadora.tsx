b2ea186b1efef287d5e103d1304dc6e306efa698
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-20 16:58:58 -0300

    parte da Locadora feita

--- src/Pages/locadora/locadora.tsx
+++ src/Pages/locadora/locadora.tsx
@@ -1,6 +1,8 @@
 import '../../Assets/CSS/locadora.css'
 import { Header } from '../../Components/Header/header'
 import { Footer } from '../../Components/Footer/footer'
+import { useEffect, useState } from 'react'
+import { apiCase } from '../../services/api'
 export const Locadora = (): JSX.Element => {
     let propsLocadora: any = {
         descriptionHome: 'Home',
@@ -11,6 +13,68 @@ export const Locadora = (): JSX.Element => {
         supdescription: 'Sair',
         underlineLocadora: 'underlineLocadora',
     }
+
+    const [locadora, setLocadora] = useState<any[]>([]);
+
+    const PegandoLocadora = () => {
+
+        apiCase.get('locadoras')
+            .then(resultado => {
+                setLocadora(resultado.data)
+                
+            })
+
+    }
+
+    useEffect(() => {
+
+        PegandoLocadora()
+    }, [])
+
+    const ExcluirLocadora = (id: any) => {
+        if (window.confirm('Deseja realmente excluir o Perfil?')) {
+            apiCase.delete(`locadoras/${id}`)
+            .then(() => {
+                window.location.reload()
+                 
+
+                })
+            }
+            
+    }
+
+
+
+    const TrazerDadosDoLocadora = (id:number, nome:string , ) => {
+
+        setNomeLocadoras(nome)
+        setGuardaIdLocadoras(id)
+
+    }
+    window.scroll({top:
+        310,left: 0,behavior: 'smooth'})
+
+
+        // const feita para guarda o Id e la em baixo no Onclick
+    const [guardaIdLocadoras, setGuardaIdLocadoras] = useState(0);
+
+    const [nomeLocadoras, setNomeLocadoras] = useState('');
+   
+    
+    const EditarLocadora = (id: number) => {
+
+            if(nomeLocadoras !== ''){
+
+                apiCase.put(`locadoras/${id}`, { nome: nomeLocadoras })
+                .then(() => {
+                    window.location.reload()
+                    
+                })
+                
+            }   
+    }
+
+
     return (
         <>
                  <Header objeto={propsLocadora} />
@@ -19,32 +83,44 @@ export const Locadora = (): JSX.Element => {
                     <h2 id='tituloLocadora'>Locadora</h2>
                     <div className='inputsLocadora'>
                         <div className='centralizarInputslocadora'>
-                            <input placeholder='Locadora:' className='inputCadastrarLocadora' type="text" />
+                            <input 
+                            placeholder='Locadora:' 
+                            className='inputCadastrarLocadora' 
+                            type="text" 
+                            defaultValue={nomeLocadoras}
+                            onChange={e => setNomeLocadoras(e.target.value)}
+                            />
                         </div>
                         <div className='centralizarInputslocadora'>
                             <input placeholder='Endereço:' className='inputCadastrarLocadora' type="text" />
                         </div>
                         <div id='centralizarInputBotao'>
                             <input placeholder='Telefone:' className='inputCadastrarLocadora telefoneLocadora' type="text" />
-                            <button className='botaoCadastrarLocadora'>Cadastrar</button>
+                            <button className='botaoCadastrarLocadora'onClick={() => EditarLocadora(guardaIdLocadoras)}>Cadastrar</button>
                         </div>
                     </div>
                 </section>
+              
+                {locadora.map((item) : any =>{
+                    return(
+
                 <section className='SectionCardsLocadora'>
                     <div className='cardsLocadora'>
                         <div className='CentralizarNomeLocadora'>
-                            <h3 className='nomeLocadora'>Locadora Matriz</h3>
+                            <h3 className='nomeLocadora'>{item.nome}</h3>
                         </div>
                         <div className='centralizarInformacoesLocadora'>
                             <p className='informacoesLocadora'>Rua marques, 320</p>
                             <p className='informacoesLocadora'>(11) 8888-8888</p>
                         </div>
                         <div className='botoesLocadora'>
-                            <button className='botaoEditar'>Editar</button>
-                            <button className='botaoExcluir'>Excluir</button>
+                            <button className='botaoEditar'onClick={() => TrazerDadosDoLocadora (item.id , item.nome)}>Editar</button>
+                            <button className='botaoExcluir'onClick={() => ExcluirLocadora(item.id)}>Excluir</button>
                         </div>
                     </div>
                 </section>
+                    )
+                })}
             </div>
             <Footer />
         </>
