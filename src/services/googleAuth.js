6e931d107bcc7d3b8ff321b8e3a29f459815746f
Vinicius Gomes <98960590+viniciusg34@users.noreply.github.com>
2022-05-24 15:42:29 -0300

    fix auth

--- src/services/googleAuth.js
+++ src/services/googleAuth.js
@@ -6,18 +6,17 @@ export function HandleCredentialResponse(response) {
     let token = parseJwt(response.credential)
     localStorage.setItem('token', JSON.stringify(token))
     console.log(token)
-    
   }
 
-    window.google.accounts.id.initialize({
-      client_id: "671297347573-1m7mp3sei7iphdom1pa6cfvfg10dobv2.apps.googleusercontent.com",
-      callback: HandleCredentialResponse
-    });
-    window.google.accounts.id.renderButton(
-      document.getElementById("buttonDiv"),
-      { theme: "outline", size: "large", width: 400}  // customization attributes
-    );
-    window.google.accounts.id.prompt(); // also display the One Tap dialog
+  window.google.accounts.id.initialize({
+    client_id: "671297347573-1m7mp3sei7iphdom1pa6cfvfg10dobv2.apps.googleusercontent.com",
+    callback: HandleCredentialResponse
+  });
+  window.google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large", width: 400}  // customization attributes
+  );
+  window.google.accounts.id.prompt(); // also display the One Tap dialog
   console.log(window.google.accounts)
 }
 
@@ -27,6 +26,9 @@ function parseJwt (token) {
   // let redirect = localStorage.getItem('token')
   if(decoded.email === "viniciusgomesdecerqueira@gmail.com" || decoded.email === "alexsandrosouzadasilva75@gmail.com" ){
     window.location.replace('http://localhost:3000/carros')
+  }
+  else{
+    window.location.replace('http://localhost:3000/perfilUsuario')
   }
   return decoded;
 };
