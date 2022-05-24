321bcbb001ceff8e301ba6df1b191c0ea177016f
ArloSindorff <98981849+ArloSindorff@users.noreply.github.com>
2022-05-24 15:24:40 -0300

    iniciando o carrosUsuarios

---
+++ src/Pages/CarrosUsuario/carrosUsuario.tsx
@@ -0,0 +1,189 @@
+// Imagens Header e Footer
+import Header from "../../Components/Header/header"
+import Footer from "../../Components/Footer/footer"
+
+// Imagens dos Carros
+import car3_2 from '../../Assets/IMGsCarros/car3_2.png'
+import car2_2 from '../../Assets/IMGsCarros/car2_2.png'
+import car1_1 from '../../Assets/IMGsCarros/car1_1.png'
+
+// Css
+import '../../Assets/CSS/carros.css'
+import '../../Assets/CSS/carrosUsuarios.css'
+
+
+export const CarrosUsuario = ():JSX.Element => {
+    
+    let propsLocadora: any = {
+        descriptionHome: 'Home',
+        descriptionCarros: 'Carros',
+        descriptionReservas: 'Reservas',
+        descriptionLocadora: 'Locadora',
+        descriptionPerfil: 'Perfil',
+        supdescription: 'Sair',
+        underlineLocadora: 'underlineLocadora',
+    }
+    
+    return(
+        <>
+           
+            <div>
+                <Header objeto={propsLocadora} />
+                <div className='mainCarros espaçamentoFinalCarros'>
+                    <div className='alinhamentoMainCarros'>
+                        
+                        <div>
+                            <h1 className="divH1Carros">Carros</h1>
+                        </div>
+
+                        {/* Econômico */}
+                        
+                        <section className='sectionsDoscardscarros espaçamentoEntreAsSections'>
+                            <h2 className='h2Carros'>Econômico</h2>
+                            <div className="barraDeSeparacaoCarros"></div>
+
+
+                                <div className='alinhamentoDosCards'>
+                                    
+                                        
+                                    <div className='cardsAPICarrosUsuarios'>
+                                        <img src={car3_2} alt="Imagem de um carro econômico" />
+                                        <div className='alinhamentocardCarros'>
+                                            <div className='h3CardDiv'>
+                                                <h3 className='nomeDoCarro'>nome do carro</h3>
+                                            </div>
+                                            <div className='divPCardCarros'>
+                                                <p className='pCardCarros'>Faça a sua reserva e garata a locação do automóvel.</p>
+                                            </div>
+                                            <div className='buttonsDoCardcarro'>
+                                                <button className='buttonDetalhesCarros'>Detalhes</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                        
+                                    
+                                </div>
+                        </section>
+
+                        {/* Especial */}
+
+                        <section className='sectionsDoscardscarros'>
+                            <h2 className='h2Carros'>Especial</h2>
+                            <div className="barraDeSeparacaoCarros"></div>
+
+                            <div className='alinhamentoDosCards'>
+                                <div className='cardsAPICarrosUsuarios'>
+                                    <img src={car2_2} alt="Imagem de um carro econômico" />
+                                    <div className='alinhamentocardCarros'>
+                                        <div className='h3CardDiv'>
+                                            <h3 className='nomeDoCarro'>Fiat Uno 1.0</h3>
+                                        </div>
+                                        <div className='divPCardCarros'>
+                                            <p className='pCardCarros'>Faça a sua reserva e garata a locação do automóvel.</p>
+                                        </div>
+                                        <div className='buttonsDoCardcarro'>
+                                            <button className='buttonDetalhesCarros'>Detalhes</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='cardsAPICarrosUsuarios'>
+                                    <img src={car2_2} alt="Imagem de um carro econômico" />
+                                    <div className='alinhamentocardCarros'>
+                                        <div className='h3CardDiv'>
+                                            <h3 className='nomeDoCarro'>Fiat Uno 1.0</h3>
+                                        </div>
+                                        <div className='divPCardCarros'>
+                                            <p className='pCardCarros'>Faça a sua reserva e garata a locação do automóvel.</p>
+                                        </div>
+                                        <div className='buttonsDoCardcarro'>
+                                            <button className='buttonDetalhesCarros'>Detalhes</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='cardsAPICarrosUsuarios'>
+                                    <img src={car2_2} alt="Imagem de um carro econômico" />
+                                    <div className='alinhamentocardCarros'>
+                                        <div className='h3CardDiv'>
+                                            <h3 className='nomeDoCarro'>Fiat Uno 1.0</h3>
+                                        </div>
+                                        <div className='divPCardCarros'>
+                                            <p className='pCardCarros'>Faça a sua reserva e garata a locação do automóvel.</p>
+                                        </div>
+                                        <div className='buttonsDoCardcarro'>
+                                            <button className='buttonDetalhesCarros'>Detalhes</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </section>
+
+                        {/* Luxo */}
+
+                        <section className='sectionsDoscardscarros'>
+                            <h2 className='h2Carros'>Luxo</h2>
+                            <div className="barraDeSeparacaoCarros"></div>
+
+                            <div className='alinhamentoDosCards'>
+                                <div className='cardsAPICarrosUsuarios'>
+                                    <img src={car1_1} alt="Imagem de um carro econômico" />
+                                    <div className='alinhamentocardCarros'>
+                                        <div className='h3CardDiv'>
+                                            <h3 className='nomeDoCarro'>Fiat Uno 1.0</h3>
+                                        </div>
+                                        <div className='divPCardCarros'>
+                                            <p className='pCardCarros'>Faça a sua reserva e garata a locação do automóvel.</p>
+                                        </div>
+                                        <div className='buttonsDoCardcarro'>
+                                            <button className='buttonDetalhesCarros'>Detalhes</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='cardsAPICarrosUsuarios'>
+                                    <img src={car1_1} alt="Imagem de um carro econômico" />
+                                    <div className='alinhamentocardCarros'>
+                                        <div className='h3CardDiv'>
+                                            <h3 className='nomeDoCarro'>Fiat Uno 1.0</h3>
+                                        </div>
+                                        <div className='divPCardCarros'>
+                                            <p className='pCardCarros'>Faça a sua reserva e garata a locação do automóvel.</p>
+                                        </div>
+                                        <div className='buttonsDoCardcarro'>
+                                            <button className='buttonDetalhesCarros'>Detalhes</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='cardsAPICarrosUsuarios'>
+                                    <img src={car1_1} alt="Imagem de um carro econômico" />
+                                    <div className='alinhamentocardCarros'>
+                                        <div className='h3CardDiv'>
+                                            <h3 className='nomeDoCarro'>Fiat Uno 1.0</h3>
+                                        </div>
+                                        <div className='divPCardCarros'>
+                                            <p className='pCardCarros'>Faça a sua reserva e garata a locação do automóvel.</p>
+                                        </div>
+                                        <div className='buttonsDoCardcarro'>
+                                            <button className='buttonDetalhesCarros'>Detalhes</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </section>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </>
+    )
+}
