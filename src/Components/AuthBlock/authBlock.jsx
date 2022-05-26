df645b53c208ad7c1aeef5e2949d082cbe5303e2
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-26 18:29:46 -0300

    home

--- src/Components/AuthBlock/authBlock.jsx
+++ src/Components/AuthBlock/authBlock.jsx
@@ -3,6 +3,7 @@ import { Navigate } from "react-router-dom";
 export const ProtectedRoute = ({ user, children }) => {
   let token = localStorage.getItem("token")
   if (token === undefined) {
+    return <Navigate to="/" replace />;
   }
 
   return children;
