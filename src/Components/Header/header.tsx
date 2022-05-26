eff5d5e597825fcd3c7d9faa2933a47386a33522
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-26 18:23:57 -0300

    Pull do vini

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,7 +1,7 @@
 import '../../Assets/CSS/header.css'
 import { Link } from "react-router-dom";
 import LogoHeader from "../../Assets/img/logo.png"
-import Logout from '../Logout/logout';
+import Logout from '../logout/logout';
 export const Header = (props:any): JSX.Element => {
 
 
