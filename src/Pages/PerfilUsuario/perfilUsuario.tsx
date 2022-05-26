9fa21cc57239c53874fa8fb6741797c6d11ee6c5
NitroAS <98990207+NitroAS@users.noreply.github.com>
2022-05-26 20:39:38 -0300

    Parte Exxluir e Editar PerfilUsuario

--- src/Pages/PerfilUsuario/perfilUsuario.tsx
+++ src/Pages/PerfilUsuario/perfilUsuario.tsx
@@ -4,6 +4,7 @@ import { Footer } from '../../Components/Footer/footer'
 import { useEffect, useState } from 'react'
 import { apiCase } from '../../services/api'
 import Swal from 'sweetalert2'
+
 export const PerfilUsuario = (): JSX.Element => {
     let propsPerfilUsuario: any = {
         descriptionCarrosUsuario: 'Carros',
@@ -27,26 +28,54 @@ export const PerfilUsuario = (): JSX.Element => {
     useEffect(() => {
         PegandoPerfisUsuario()
     }, [])
-
+    useEffect(() => {
+        BuscarUsuarios()
+    }, [perfisUsuario])
     
 
-    const ExcluirPerfilUsario = (id: any) => {
-        if (window.confirm('Deseja realmente Excluir o Perfil?')) {
-            apiCase.delete(`usuario/${id}`)
-                .then(() => {
-                    window.location.reload()
-
-                })
-            }
+    const BuscarUsuarios = () =>  {
             
+        const A:any = localStorage.getItem('token')
+
+        const token:any  = JSON.parse(A)
+        
+        const usuario = perfisUsuario.find(usuario => usuario.email !== token.email)
+        
+
+        if (usuario !== undefined) {
+            setGuardaIdUsuario(usuario.id)
+            setNomeEmailUsuario(usuario.email)
+            setTelefoneUsuario(usuario.telefone)
+            setNomePerfisUsuario(usuario.nome)
+        }
+      
+
     }
 
-    const TrazerDadosDoPerfilUsuario = (id:number, nome:string , email:string , telefone:string) => {
-        setNomePerfisUsuario(nome)
-        setNomeEmailUsuario(email)
-        setGuardaIdUsuario(id)
-        setTelefoneUsuario(telefone)
+    const ExcluirPerfilUsario = (id: any) => {
+
+        Swal.fire({
+            title: 'Deseja Excluir esse Perfil??',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Delete!'
+        })
+        .then((resultado) => {
+            if (resultado.isConfirmed) {
+
+                apiCase.delete(`usuario/${id}`)
+                    .then(() => {
+                        window.location.reload()
+    
+                    })
+
+             }
+        })
+                   
     }
+
     window.scroll({top:
         310,left: 0,behavior: 'smooth'})
         // const feita para guarda o Id e la em baixo no Onclick
@@ -55,7 +84,16 @@ export const PerfilUsuario = (): JSX.Element => {
     const [nomeEmailUsuario, setNomeEmailUsuario] = useState('');
     const [telefoneUsuario, setTelefoneUsuario] = useState('')
 
+    const GuardaHook = (id:any , nome:string , email:string , telefone:string) => {
+        setGuardaIdUsuario(id)
+        setNomePerfisUsuario(nome)
+        setNomeEmailUsuario(email) 
+        setTelefoneUsuario(telefone) 
+    
+      }
+
     const EditarPerfilUsuario = (id: number) => {
+        
             if(nomePerfisUsuario !== '' && nomeEmailUsuario !== '' && telefoneUsuario !== ''){
                 apiCase.put(`usuario/${id}`, { nome: nomePerfisUsuario, email: nomeEmailUsuario , telefone: telefoneUsuario })
                 .then(() => {
@@ -65,39 +103,6 @@ export const PerfilUsuario = (): JSX.Element => {
             }   
     }
 
-    const CadastrarPerfilUsuario = () => {
-
-        for (let index = 0; index < perfisUsuario.length; index++) {
-            if(perfisUsuario[index].nome === nomePerfisUsuario)
-            {
-                return
-            }
-        }
-
-
-        if (nomePerfisUsuario !== '' && nomeEmailUsuario !== '' && telefoneUsuario !== '') {
-
-            apiCase.post(`usuario`, {nome: nomePerfisUsuario, email: nomeEmailUsuario, telefone: telefoneUsuario})
-            .then(() => {
-                window.location.reload()
-                
-            })
-        }
-
-        else {
-            Swal.fire({
-                title: 'Por Favor, Preencha os campos vazios',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#DB1812',
-                cancelButtonColor: '#41B8D2',
-                confirmButtonText: 'OK'
-            })
-        }
-        
-     
-    }
-   
     return (
         <>
             <Header objeto={propsPerfilUsuario} />
@@ -149,27 +154,11 @@ export const PerfilUsuario = (): JSX.Element => {
                                 <button className='btnPerfilAtualizarUsuario' onClick={() => EditarPerfilUsuario(guardaIdUsuario)}>Atualizar</button>
                             </div>
                             <div className="btnExcluirAlinhamentoPerfilUsuario">
-                                <button className='btnPerfilExcluirUsuario'>Excluir Cadastro</button>
+                                <button className='btnPerfilExcluirUsuario'onClick={() => ExcluirPerfilUsario(guardaIdUsuario)}>Excluir Cadastro</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* <div className='alinhamentoPerfilFinal'>
-                    {perfisUsuario.map((item): any => {
-                        return (
-                            <div className="bordaPerfil" onClick={() => TrazerDadosDoPerfilUsuario (item.id , item.nome , item.email, item.telefone )}>
-                                <div className="container">
-                                    <div className="boxCadastro">
-                                        <p className='pTituloPerfil'>{item.nome}</p>
-                                        <p className='pMsgPerfil'>{item.telefone}</p>
-                                        <p className='pMsgPerfil'>{item.email}</p>
-                                        <button className='excluirCadastroPerfil' onClick={() => ExcluirPerfilUsario( item.id)}>Excuir usuário</button>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div> */}
             </div>
             <Footer />
         </>
